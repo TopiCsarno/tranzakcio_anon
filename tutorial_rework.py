826e909f0735a1de4475cbcefd06e000a0a967e8
Csarnó <tamas.csarno@gmail.com>
2021-06-27 15:42:29 +0200

    tutorial rework

--- tutorial_rework.py
+++ tutorial_rework.py
@@ -58,7 +58,7 @@ fig.show()
 # %% [markdown]
 #  ## 2. rész
 #  - egy perturb
-#  - több deanon összehasonlítása (nar, blb, grh, KL, DV?)
+#  - több deanon összehasonlítása (nar, blb, KL)
 #  - accuracy mennyi
 #  - scoreboard
 
@@ -66,28 +66,26 @@ fig.show()
 # paraméterek
 experiment = '2_exp'
 network = 'enron'
-size = 3000
+size = 2000
 
 # %%
-# újatt háttérismeret generálása
-create_data(network,size,experiment, 1, 'ns09', 0.8, 0.8, verbose=True)
+# új háttérismeret generálása
+create_data(network, size, experiment, 1, 'ns09', 0.8, 0.8)
 
 # %%
-
 # Három deanon algoritmus (ki lehet fejteni szövegesen)
-
-print('ez az ns09 algo')
+print('ns09 deanonimizáció futtatása')
 simulate(network, size, experiment, 'ns09', 'random.25', 50, 0.01)
 # ez a blb, mit tud?
 # %%
-print('ez az blb algo')
+print('blb deanonimizáció futtatása')
 simulate(network, size, experiment, 'blb', 'random.25', 50, '0.1,0.5')
 # %%
-print('ez az kl algo')
-simulate(network, size, experiment, 'KL', 'random.25', 50, 50)
+print('KL deanonimizáció futtatása')
+simulate(network, size, experiment, 'KL', 'random.25', 50, 100)
 
 # %%
-# pontosságok beolvasása
+print('pontosságok beolvasása')
 df = pd.DataFrame(columns=['experiment', 'anon', 'deanon', 'TPR', 'FPR'])
 for deanon in ['ns09', 'blb', 'KL']:
 	TPR, _, FPR, _ = read_accuracy(network, size, experiment, deanon)['avg']
@@ -111,25 +109,28 @@ fig.show()
 # paraméterek
 experiment = '3_exp'
 deanon = 'ns09'
-network = 'wiki'
-size = 2000
-# %%
+network = 'enron'
+size = 2000 
+
+print("Háttérismeret generálása")
 create_data(network, size, experiment, 1, 'ns09', 0.5, 0.75)
-# %%
+
 # define parameters
 params = {
-    'sw': 0.1,
-    'kda': 50,
+    'sw': 0.2,
+    'kda': 5,
     'dp': 50
 }
 
 # results will be sroted in this df
 df = pd.DataFrame(columns=['experiment', 'anon', 'deanon', 'TPR', 'FPR', 'utility'])
 
-for anon in ['sw', 'kda', 'dp']:
+for anon in ['none', 'sw', 'kda', 'dp']:
     # anonimize graph
-    anonimize(network, size, experiment, anon, params[anon])
-    
+    if anon != 'none':
+        print("Anonimizálás {} módszerrel".format(anon))
+        anonimize(network, size, experiment, anon, params[anon])
+
     # calculate utility loss
     util = utility(network, size, experiment, 'lcc')
 
@@ -139,7 +140,7 @@ for anon in ['sw', 'kda', 'dp']:
     # calculate results
     TPR, _, FPR, _ = read_accuracy(network, size, experiment, deanon)['avg']
     df = df.append({'experiment':experiment, 'anon':anon, 'deanon':deanon, 'TPR':TPR, 'FPR':FPR, 'utility':util}, ignore_index=True)
-
+print("Eredmények értékelése")
 df
 
 # %%
@@ -161,8 +162,7 @@ fig.show()
 # utility
 # %%
 # paraméterek
-experiment = '4_exp'
-network = 'wiki'
+network = 'enron'
 size = 1000
 nseed = 50
 
@@ -175,39 +175,50 @@ params = {
     'dp': 50,
     # deanon
     'ns09': 0.01,
-    'KL': 50,
+    'KL': 100,
     'blb': '0.1,0.5',
     # util
     'inf': 5
 }
 
-# adott háttérismeret
-create_data(network, size, experiment, 'ns09', 1, 0.5, 0.75)
+# Háttérismeret generálása
+print('Gyenge támadó feltételezve')
+create_data(network, size, "gyenge", 1, 'ns09', 0.4, 0.6)
 
-# results will be sroted in this df
-df = df_util = pd.DataFrame()
+print('Erős támadó feltételezve')
+create_data(network, size, "eros", 1, 'ns09', 0.8, 0.8)
 
-for anon in ['sw', 'kda', 'dp']:
-    # anonimize graph
-    anonimize(network, size, experiment, anon, params[anon])
-    
-    # calculate utility loss for anonimization method
-    for util in ['inf', 'deg', 'lcc']:
-        param = params.get(util, None)
-        value = utility(network, size, experiment, util, param)
-        df_util = df_util.append({'anon':anon, 'value':value, 'util': util}, ignore_index=True)
+# eredmények gyűjtéséhez létrehozott táblázat
+df = df_util = pd.DataFrame(columns=['experiment', 'anon', 'deanon', 'TPR', 'FPR'])
 
-    for deanon in ['ns09', 'KL', 'blb']:
-        # run deanon algos
-        simulate(network, size, experiment, deanon, 'random.25', nseed, params[deanon])
-        # calculate results
-        TPR, _, FPR, _ = read_accuracy(network, size, experiment, deanon)['avg']
-        df = df.append({'experiment':experiment, 'anon':anon, 'deanon':deanon, 'TPR':TPR, 'FPR':FPR}, ignore_index=True)
+# Egyes kombilációk futtatása
+for experiment in ['gyenge', 'eros']:
+    print('\n'+experiment)
+    for anon in ['none', 'sw', 'kda', 'dp']:
+        print(anon)
+        # anonimize graph
+        if (anon != 'none'):
+            anonimize(network, size, experiment, anon, params[anon])
+        
+            # calculate utility loss for anonimization method
+            # for util in ['inf', 'deg', 'lcc']:
+            #     param = params.get(util, None)
+            #     value = utility(network, size, experiment, util, param)
+            #     df_util = df_util.append({'anon':anon, 'value':value, 'util': util}, ignore_index=True)
+
+        for deanon in ['ns09', 'KL', 'blb']:
+            print('\t'+deanon)
+            # run deanon algos
+            simulate(network, size, experiment, deanon, 'random.25', nseed, params[deanon])
+            # calculate results
+            TPR, _, FPR, _ = read_accuracy(network, size, experiment, deanon)['avg']
+            df = df.append({'experiment':experiment, 'anon':anon, 'deanon':deanon, 'TPR':TPR, 'FPR':FPR}, ignore_index=True)
 df
 
 # %%
 # Scoreborad
-fig = px.scatter(df, x='FPR', y='TPR', 
+data = df[df['experiment']=='gyenge']
+fig = px.scatter(data, x='FPR', y='TPR', 
     color='deanon',
     symbol='anon',
     range_y=[0,100],
@@ -215,18 +226,31 @@ fig = px.scatter(df, x='FPR', y='TPR',
         "FPR": "Incorrect matches (FPR)",
         "TPR": "Correct matches (TPR)",
     },
-    title="Scoreboard: Correct vs Incorrect matches")
+    title="Gyenge támadó modellt feltételezve")
 fig.show()
 # %%
-# Change the default stacking
-fig = px.bar(df_util, x="anon", y="value",
-    color='util', 
-    barmode='group',
+# Scoreborad
+data = df[df['experiment']=='eros']
+fig = px.scatter(data, x='FPR', y='TPR', 
+    color='deanon',
+    symbol='anon',
+    range_y=[0,100],
     labels={
-        "util": "Utility",
-        "anon": "Anonimization technique",
+        "FPR": "Incorrect matches (FPR)",
+        "TPR": "Correct matches (TPR)",
     },
-    title="Utility loss for each technique")
+    title="Erős támadó modellt feltételezve")
 fig.show()
-
 # %%
+# # Change the default stacking
+# fig = px.bar(df_util, x="anon", y="value",
+#     color='util', 
+#     barmode='group',
+#     labels={
+#         "util": "Utility",
+#         "anon": "Anonimization technique",
+#     },
+#     title="Utility loss for each technique")
+# fig.show()
+
+# # %%
